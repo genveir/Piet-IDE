017c935cf7e56ac1c6e16991523c3ada23c12692
Geerten Vink <maedaros@yahoo.com>
2020-12-11 12:45:48 +0100

    file stuff to own file

---
+++ Piet IDE/MainWindow.Files.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Piet_IDE
+{
+    partial class MainWindow
+    {
+        private void SaveStateHandler(object sender, EventArgs e)
+        {
+            var saveDialog = new SaveFileDialog();
+            saveDialog.InitialDirectory = @"d:\temp\piet";
+            saveDialog.Filter = "Bitmap|*.bmp";
+            saveDialog.FileOk += SaveDialog_FileOk;
+            saveDialog.ShowDialog();
+        }
+        private void SaveDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            var saveDialog = (SaveFileDialog)sender;
+            drawingState.Save(saveDialog.FileName);
+        }
+
+        private void LoadStateHandler(object sender, EventArgs e)
+        {
+            var openDialog = new OpenFileDialog();
+            openDialog.InitialDirectory = @"d:\temp\piet";
+            openDialog.FileOk += Dialog_FileOk;
+            openDialog.ShowDialog();
+        }
+        private void Dialog_FileOk(object sender, CancelEventArgs e)
+        {
+            var dialog = (OpenFileDialog)sender;
+            drawingState = DrawingState.Load(dialog.FileName);
+
+            this.Invalidate();
+        }
+    }
+}
--- Piet IDE/MainWindow.cs
+++ Piet IDE/MainWindow.cs
@@ -52,35 +52,6 @@ namespace Piet_IDE
 
         private DrawingState drawingState;
 
-        private void SaveStateHandler(object sender, EventArgs e)
-        {
-            var saveDialog = new SaveFileDialog();
-            saveDialog.InitialDirectory = @"d:\temp\piet";
-            saveDialog.Filter = "Bitmap|*.bmp";
-            saveDialog.FileOk += SaveDialog_FileOk;
-            saveDialog.ShowDialog();
-        }
-        private void SaveDialog_FileOk(object sender, CancelEventArgs e)
-        {
-            var saveDialog = (SaveFileDialog)sender;
-            drawingState.Save(saveDialog.FileName);
-        }
-
-        private void LoadStateHandler(object sender, EventArgs e)
-        {
-            var openDialog = new OpenFileDialog();
-            openDialog.InitialDirectory = @"d:\temp\piet";
-            openDialog.FileOk += Dialog_FileOk;
-            openDialog.ShowDialog();
-        }
-        private void Dialog_FileOk(object sender, CancelEventArgs e)
-        {
-            var dialog = (OpenFileDialog)sender;
-            drawingState = DrawingState.Load(dialog.FileName);
-
-            this.Invalidate();
-        }
-
         private void MainWindow_Shown(object sender, System.EventArgs e)
         {
             img = new Bitmap(this.Width, this.Height);
